0385f89e2b4eb243a3e103898629d718091017d0
kkerwin <kkerwin@yeah.net>
2021-08-13 14:56:48 +0800

    electron

---
+++ electron-app/src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+import React, { FC } from 'react'
+
+const Home:FC = () => {
+    return <h1>
+        dashboard !!!
+    </h1>
+}
+
+export default Home
--- re-webpack/src/pages/index/music.tsx
+++ re-webpack/src/pages/index/music.tsx
@@ -1,12 +1,44 @@
-import { FC } from "react"
+import { ChangeEvent, FC, useCallback } from "react"
 import { Audio } from '@/components'
+import { useImmer } from '@/hooks'
+import { debounce } from '@/utils'
  
 
+interface Istate {
+    palyed:number,
+    total:number
+    isPaly:boolean
+    interval:null | number
+}
+
 const Music:FC = () => {
+
+    const [ time, setTime ] = useImmer({
+        palyed:0,
+        total:100,
+        isPaly:false,
+        interval:null
+    })
+
+    const play = useCallback(debounce((v:number) => {
+       
+        
+    },500),[])
+
+    const handleRangeChange = (e:ChangeEvent<HTMLInputElement>) => {
+        e.persist()    
+        const v = e.target.valueAsNumber
+        play(v)    
+        setTime(draft => {
+            draft.palyed = v
+        })
+    }
 
     return <div>
         <h1>come some of the music</h1>
         <Audio />
+
+        {/* <input type="range" style={{ width:500 }} onChange={handleRangeChange} min={0} value={time.palyed} max={time.total} /> */}
     </div>
 }
 
