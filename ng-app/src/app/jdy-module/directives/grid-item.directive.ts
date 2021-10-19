8fd170155dc227b60a1b10b81e050d26d0886f0e
kkerwin <kkerwin@yeah.net>
2021-10-19 22:42:21 +0800

    HostBinding & HostListener

--- ng-app/src/app/jdy-module/directives/grid-item.directive.ts
+++ ng-app/src/app/jdy-module/directives/grid-item.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: '[appGridItem]'
@@ -7,6 +7,7 @@ export class GridItemDirective implements OnInit {
 
     @Input() appGridItem: string = '100px'
     @Input() fitMode: string = 'none'
+    @Input() @HostBinding('style.text-align') ta = 'left'
     constructor(private elr: ElementRef, private rd2: Renderer2) { }
 
     ngOnInit() {
@@ -16,4 +17,9 @@ export class GridItemDirective implements OnInit {
         this.rd2.setStyle(this.elr.nativeElement, 'object-fit', this.fitMode)
     }
 
+    @HostListener('click', ['$event.target'])
+    handleClick(ev: any) {
+        console.log(ev.innerHTML);
+
+    }
 }
