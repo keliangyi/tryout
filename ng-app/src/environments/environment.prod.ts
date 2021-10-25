d8a2f12008157bab8955471973cbf19fc52f2fca
kkerwin <kkerwin@yeah.net>
2021-10-25 18:10:42 +0800

    jsonMasterKey

--- ng-app/src/environments/environment.prod.ts
+++ ng-app/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.jsonbin.io/v3'
+    apiUrl: 'https://api.jsonbin.io/v3',
+    jsonMasterKey: '$2b$10$z5Q3jOQLX4iU/Ajpc6HFOOBHQX3FEZH7imRH.Nbq3IIJy9TJjz3Fu'
 };
 
 
