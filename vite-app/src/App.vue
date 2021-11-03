8e04066681022034a6b3b912a09e29271b79c727
kkerwin <kkerwin@yeah.net>
2021-11-03 23:27:48 +0800

    vue

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -2,12 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Test1 from './components/Test1.vue'
 </script>
 
 <template>
 	<div>
         <img alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Hello Vue 3 + Vite" />
+	    <HelloWorld msg="Hello Vue 3 + Vite" />
+        <Test1 />
     </div>
 </template>
 
---
+++ vite-app/src/components/Test1.vue
@@ -0,0 +1,41 @@
+
+<template>
+   <div>
+        <h1 @click="changeMsg()">{{ msg }}</h1>  
+        <h1 >{{ reverse }}</h1>  
+        <h1 :[key]="'red'">21</h1>
+        <input type="text" v-model="msg">
+   </div>
+</template>
+<script>
+export default {
+    name:"test",
+    data(){
+        return {
+            msg:"kly",
+            key:'color'
+        }
+    },
+    
+    methods:{
+        changeMsg() {
+            console.log(this);
+            this.msg = 'sb'
+            this.key = 'id'
+        }
+    },
+    computed:{
+        reverse(){
+            return this.msg.split('').reverse().join('')
+        }
+    },
+    watch:{
+        msg(newVal, oldVal){
+            if(newVal.length > 5){
+                alert('不能大于五')
+                this.msg = oldVal
+            }
+        }
+    }
+}
+</script>
