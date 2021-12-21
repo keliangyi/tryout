c42d72668b12cb83337256c92438fb4a5c93698b
kkerwin <kkerwin@yeah.net>
2021-12-21 22:27:29 +0800

    nestjs

--- nest-app/src/app.controller.ts
+++ nest-app/src/app.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
     constructor(private readonly appService: AppService) {
         console.log(123);
 
     }
 
-    @Get()
+    @Get('hello')
     getHello(): string {
         console.log(456);
         return this.appService.getHello();
---
+++ nest-app/src/user/user.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+
+export interface User {
+    name: string,
+    id: string
+    age: number
+}
+
+@Injectable()
+export class UserService {
+    private users: User[] = [
+        { id: '001', name: "maomao", age: 22 },
+        { id: '002', name: "admin", age: 50 },
+    ]
+
+    genUUid() {
+        return Date.now().toString(16)
+    }
+
+    getUsers(name?: string, age?: number): Array<User> {
+
+
+        return this.users.filter(f => {
+            let res = true
+            if (name && age) {
+                res = f.name.includes(name) || f.age === age
+            }
+            if (name) {
+                res = f.name.includes(name)
+            }
+            if (age) {
+                res = f.age === age
+            }
+            return res
+        })
+    }
+
+    getUserById(uid: string): User {
+        const usr = this.users.find(f => f.id === uid)
+        if (!usr) {
+            throw new NotFoundException('用户不存在',)
+        }
+        return usr
+    }
+
+    createUser(name: string, age: number) {
+        const newUser = { name, age, id: this.genUUid() }
+        this.users.push(newUser)
+        return newUser
+    }
+
+
+}
