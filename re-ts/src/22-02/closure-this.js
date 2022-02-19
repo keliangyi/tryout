9fb6ae8280dbab3087c5b8780ec76ad445ef0b4d
kkerwin <kkerwin@yeah.net>
2022-02-19 23:13:26 +0800

    this in closure

---
+++ re-ts/src/22-02/closure-this.js
@@ -0,0 +1,37 @@
+
+// 'use strict'
+
+const obj = {
+    name: 'maomao',
+    sayName() {
+        console.log(`hello my name is ${this.name}`);
+        return function () {
+            console.log(this.name) // undefined
+        }
+    },
+    sayNameArrow() {
+        console.log(`hello my name is ${this.name}`);
+        return () => {
+            console.log(this.name) // maomao
+        }
+    },
+    anotherSayName() {
+        return function inner() {
+            this.name = 'inner' // 这个this 是只想 window 的 === window.name = 'inner'
+            return function () {
+                // 这里的this 也是 window，但是 上面的inner 中给 window 添加了 name 属性
+                console.log(this.name) // inner
+            }
+        }
+    }
+}
+
+
+// const fn1 = obj.sayName
+// fn1() // === fn1.call(undefined,...)
+
+const fn2 = obj.sayName()
+fn2()
+
+const fn3 = obj.anotherSayName()()()
+
