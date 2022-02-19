333b230a8abe5d25bb8e828ad54d726f1a7a0228
kkerwin <kkerwin@yeah.net>
2022-02-19 23:15:25 +0800

    modify tips

--- re-ts/src/22-02/closure-this.js
+++ re-ts/src/22-02/closure-this.js
@@ -17,7 +17,7 @@ const obj = {
     },
     anotherSayName() {
         return function inner() {
-            this.name = 'inner' // 这个this 是只想 window 的 === window.name = 'inner'
+            this.name = 'inner' // 这个this 是指向 window 的 === window.name = 'inner'，严格模式会报错
             return function () {
                 // 这里的this 也是 window，但是 上面的inner 中给 window 添加了 name 属性
                 console.log(this.name) // inner
