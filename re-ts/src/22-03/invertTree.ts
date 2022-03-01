e8048b623c484eb4f1486d5f73ed7e7c39c0872d
kkerwin <kkerwin@yeah.net>
2022-03-01 22:54:35 +0800

    反转二叉树

---
+++ re-ts/src/22-03/invertTree.ts
@@ -0,0 +1,59 @@
+
+
+type TreeNode = {
+    value: number
+    left?: TreeNode
+    right?: TreeNode
+}
+
+const tree: TreeNode = {
+    value: 1,
+    left: {
+        value: 2,
+        left: {
+            value: 3,
+            right: {
+                value: 4
+            }
+        },
+        right: {
+            value: 5,
+            left: {
+                value: 6
+            }
+        }
+    },
+    right: {
+        value: 7,
+        left: {
+            value: 8,
+        },
+        right: {
+            value: 9,
+            left: {
+                value: 10
+            },
+            right: {
+                value: 11
+            }
+        }
+    }
+}
+
+const invertTree = (node: TreeNode) => {
+    let left = node.left
+    let right = node.right //不能用结构
+
+    node.left = right
+    node.right = left
+    if (left) {
+        invertTree(left)
+    }
+
+    if (right) {
+        invertTree(right)
+    }
+}
+
+invertTree(tree)
+console.log(JSON.stringify(tree, null, 2));
