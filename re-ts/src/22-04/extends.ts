5b9bac96199dfc4dd8ae2e56fa9a8871588a3d08
kkerwin <kkerwin@yeah.net>
2022-04-21 22:20:49 +0800

    Extract

--- re-ts/src/22-04/extends.ts
+++ re-ts/src/22-04/extends.ts
@@ -8,19 +8,59 @@ class Animal {
 
 class Parent extends Person {
 	constructor() {
-		super("maomao", "001");
+		super('maomao', '001')
 	}
 	getParent() {}
 }
 
 class Son extends Parent {
 	constructor() {
-		super();
+		super()
 	}
 }
 
-type Ex1 = Son extends Person ? true : false; // true
-type Ex2 = Person extends Son ? true : false; // false => Son 多了一个 Parent 的 getParent
-type Ex3 = Animal extends Person ? true : false; // false
+type Ex1 = Son extends Person ? true : false // true
+type Ex2 = Person extends Son ? true : false // false => Son 多了一个 Parent 的 getParent
+type Ex3 = Animal extends Person ? true : false // false
 
-export {};
+// 联合类型
+type U1 = string | number
+type U2 = string | number | symbol
+
+type N1 = 0 | 1 extends 1 | 0 ? true : false
+
+type Un = 'a' | 'b' | 'c' extends 'a' | 'b' ? 1 : 2
+type Extends<T, U> = T extends U ? 1 : 0
+
+type E1 = Extends<U1, U2> // 1,很好理解
+type E2 = Extends<U2, U1> // 0 | 1，因式分解
+/**
+ * type E2 = Extends<U2, U1>
+ *
+ * 1. type E2 = Extends<string | number, string | number | symbol>
+ * 2. type E2 = (string | number) extends (string | number | symbol) ? 1 : 0
+ * 3. type E2 = (string extends string ? 1 : 0) | (string extends number ? 1 : 0 ) |
+ *              (string extends symbol ? 1 : 0) | (number extends string ? 1 : 0) |
+ *              (number extends number ? 1 : 0 ) | (number extends symbol ? 1 : 0)
+ * 4. type E2 = 1 | 0 | 0 | 0 | 1 | 0
+ * 5. type E2 = 0 | 1
+ */
+
+//分配律
+
+type E3<T> = string extends T ? 1 : 0
+type E3_1 = E3<string | number>
+
+type E4<T, U> = U extends T ? 1 : 0
+
+type E4_1 = E4<string | number, string>
+type E4_1_1 = E4<string, number> | E4<string, string>
+type E4_1_2 = (string extends number ? 1 : 0) | (string extends string ? 1 : 0)
+type E4_1_3 = 0 | 1
+
+//==========函数============
+type F1 = (a: string, b: number) => void
+type F2 = (a: string) => void
+
+type FE1 = F2 extends F1 ? F2 : never
+export {}
--- re-ts/src/22-04/extract.ts
+++ re-ts/src/22-04/extract.ts
@@ -1,1 +1,42 @@
-type E = Extract<"a", "a" | "c">;
+type E = Extract<'a', 'a' | 'c'> // a
+
+// 应用 替换泛型约束中的extends:
+
+// 接收两个参数，并且都是对象
+/**
+ * 方法1：extends
+ * @param o1
+ * @param o2
+ * @returns
+ */
+function merge1<T extends object, U extends object>(o1: T, o2: U): T & U {
+	return { ...o1, ...o2 }
+}
+
+// merge1(1, 2) //Error
+merge1({ a: 1 }, { b: 2 })
+
+/**
+ * 方法2：Extract，
+ * 当 T 和 U extends object 时才是 T 和 U，否则就是 never
+ * @param o1
+ * @param o2
+ * @returns
+ */
+function merge2<T, U>(o1: Extract<T, object>, o2: Extract<U, object>): T & U {
+	return { ...o1, ...o2 }
+}
+
+// merge2(1, 2) //Error 类型“number”的参数不能赋给类型“never”的参数。ts(2345)
+merge2({ a: 1 }, { b: 2 })
+
+// 简化 方法2
+type MustObj<T> = Extract<T, object>
+function merge3<T, U>(o1: MustObj<T>, o2: MustObj<U>): T & U {
+	return { ...o1, ...o2 }
+}
+
+// merge3(1, 2) //Error 类型“number”的参数不能赋给类型“never”的参数。ts(2345)
+merge3({ a: 1 }, { b: 2 })
+
+export {}
