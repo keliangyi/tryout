f1f5d89183712493bf3e61826c050af3f11c3274
kkerwin <kkerwin@yeah.net>
2022-07-31 20:52:16 +0800

    算法&面试题

--- re-ts/src/22-06/99.ts
+++ re-ts/src/22-06/99.ts
@@ -2,6 +2,7 @@ const nine = () => {
 	const body = document.body
 	const wrapper = document.createElement('div')
 	wrapper.style.display = 'grid'
+
 	wrapper.style.gridTemplateColumns = 'repeat(9, 160px)'
 	wrapper.style.gridTemplateRows = 'repeat(9, 60px)'
 	const fragment = document.createDocumentFragment()
@@ -22,3 +23,31 @@ const nine = () => {
 	body.append(wrapper)
 }
 nine()
+
+const n1 = () => {
+	const body = document.body
+	const wrapper = document.createElement('div')
+	wrapper.style.display = 'grid'
+	wrapper.style.gridTemplateColumns = 'repeat(9, 160px)'
+	wrapper.style.gridTemplateRows = 'repeat(9, 60px)'
+	wrapper.style.marginTop = '48px'
+
+	for (let i = 1; i <= 9; i++) {
+		const fragment = document.createDocumentFragment()
+		for (let j = 1; j <= 9; j++) {
+			const box = document.createElement('div')
+			box.style.display = 'flex'
+			box.style.alignItems = 'center'
+			box.style.justifyContent = 'center'
+			if (j <= i) {
+				box.innerHTML = `${j}*${i} = ${j * i} `
+				box.style.border = '1px solid red'
+			}
+			fragment.append(box)
+		}
+		wrapper.append(fragment)
+	}
+
+	body.append(wrapper)
+}
+n1()
--- re-ts/src/app.ts
+++ re-ts/src/app.ts
@@ -5,9 +5,9 @@
 
 // import './ud/view'
 // import './22-06/blob'
-// import './22-06/theme'
+import './22-06/99'
 // import './22-07/log-what'
 // import './22-07/gen'
 // import './22-07/prototype'
 // import './22-07/web-compoennet'
-import './22-07/super'
+// import './22-07/super'
